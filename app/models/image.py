5adc0af13b6502e19ba25e775bdde0e020ae581f
Keith Glines <keithglines@yahoo.com>
2022-11-14 19:27:35 -0500

    Update models and seeds

--- app/models/image.py
+++ app/models/image.py
@@ -1,9 +1,12 @@
-from .db import db
+from .db import db, environment, SCHEMA
 from .user import User
 from datetime import datetime
 
 class Image(db.Model):
     __tablename__ = 'images'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
--- app/seeds/comments.py
+++ app/seeds/comments.py
@@ -5,7 +5,7 @@ def seed_comments():
         image_id=1, user_id=2, body="Looks like a great day!"
     )
     comment2 = Comment(
-        image_id=1, user_id=1, body="Thanks, it was!"
+        image_id=1, user_id=3, body="Beautiful!"
     )
     comment3 = Comment(
         image_id=2, user_id=1, body="Wow, looks great!"
@@ -14,10 +14,34 @@ def seed_comments():
         image_id=2, user_id=3, body="I'm hungry now."
     )
     comment5 = Comment(
-        image_id=3, user_id=1, body="Me too!"
+        image_id=3, user_id=2, body="Oh wow!"
     )
     comment6 = Comment(
-        image_id=3, user_id=1, body="Enjoy!"
+        image_id=3, user_id=3, body="Nope...not for me."
+    )
+    comment7 = Comment(
+        image_id=4, user_id=1, body="It's 5 o'clock somewhere!"
+    )
+    comment8 = Comment(
+        image_id=5, user_id=3, body="If you have enough beers, everything looks that way!"
+    )
+    comment9 = Comment(
+        image_id=5, user_id=2, body="That looks really cool!"
+    )
+    comment10 = Comment(
+        image_id=6, user_id=1, body="Looks fun!"
+    )
+    comment11 = Comment(
+        image_id=7, user_id=1, body="Who doesn't when the food looks that good?!"
+    )
+    comment12 = Comment(
+        image_id=8, user_id=1, body="Oh. My. God."
+    )
+    comment13 = Comment(
+        image_id=8, user_id=3, body="Yes please."
+    )
+    comment14 = Comment(
+        image_id=9, user_id=2, body="Ha, not quite!"
     )
 
     db.session.add(comment1)
@@ -26,6 +50,14 @@ def seed_comments():
     db.session.add(comment4)
     db.session.add(comment5)
     db.session.add(comment6)
+    db.session.add(comment7)
+    db.session.add(comment8)
+    db.session.add(comment9)
+    db.session.add(comment10)
+    db.session.add(comment11)
+    db.session.add(comment12)
+    db.session.add(comment13)
+    db.session.add(comment14)
 
     db.session.commit()
 
