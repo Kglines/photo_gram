2e97c9c032056addbbd051f8ad4ed55fe06ca807
Keith Glines <keithglines@yahoo.com>
2022-11-15 18:23:30 -0500

    Migration

--- migrations/versions/a40a0c01db5f_.py
+++ migrations/versions/a40a0c01db5f_.py
@@ -79,7 +79,11 @@ def upgrade():
     )
 
     if environment == "production":
-        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE users SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE images SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE comments SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE follows SET SCHEMA {SCHEMA};")
+        op.execute(f"ALTER TABLE likes SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
