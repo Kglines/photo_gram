0b196e8df909096cf3a912e8c27772db43fcbb5b
Keith Glines <keithglines@yahoo.com>
2022-11-14 22:05:21 -0500

    Another DB Upgrade

--- migrations/versions/fafdaaf5b7c0_.py
+++ migrations/versions/fafdaaf5b7c0_.py
@@ -8,6 +8,10 @@ Create Date: 2022-11-14 21:29:05.317210
 from alembic import op
 import sqlalchemy as sa
 
+import os
+environment = os.getenv("FLASK_ENV")
+SCHEMA = os.environ.get("SCHEMA")
+
 
 # revision identifiers, used by Alembic.
 revision = 'fafdaaf5b7c0'
@@ -73,6 +77,9 @@ def upgrade():
     sa.ForeignKeyConstraint(['user_id'], ['users.id'], ),
     sa.PrimaryKeyConstraint('id')
     )
+
+    if environment == "production":
+        op.execute(f"ALTER TABLE <table_name> SET SCHEMA {SCHEMA};")
     # ### end Alembic commands ###
 
 
